979ad4b402e19ec01dd65a7a16e164d24643f737
tuhong qiu <qth411599088@hotmail.com>
2020-12-17 18:27:15 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hilvdun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World方法666666!");
+        }
+    }
+}
